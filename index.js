f1cd5bdc58461becbaeeb5d1bc497707c3bbb1ab
Klove Adams <kloveadams@gmail.com>
2021-10-08 11:11:22 -0400

    Wired index to nonexistent server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// start your server here
+require("dotenv").config();
+
+const server = require("./api/server");
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+  console.log(`Runing on port ${port}`);
+});
